c5273f742e96a4904b2eb4bfd035e54c36e3c6ca
Agata Magier <agata.magier@quarticon.com>
2020-10-25 09:39:43 +0100

    replace NavLink with button in Navigation  as links dont reload page on click

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { NavList } from "../components/Header/styles"
+import styled from "styled-components";
 
+const NavButton = styled.button`
+
+  font-size: ${({ theme }) => theme.font.size.base};
+  font-weight: ${({ theme }) => theme.font.weight.bold};
+  color: ${({ theme }) => theme.color.black};
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  &:focus {
+      outline: none;
+  }
+`;
 const list = [
     {name: "Start", path: "/", exact: true },
     {name: "My profile", path: "/my-profile", }
@@ -9,22 +22,35 @@ const list = [
 
 const Navigation = () => {
     
-    // const history = useHistory();
+    const history = useHistory();
     
-    // const handleOnClick = () => {
-    //     const location: 
-    // }
-    const menu = list.map(item => (
-        <li key={item.name}>
-            <NavLink to={item.path} exact = {item.exact ? item.exact : false }>{item.name}</NavLink>
-        </li>
-    ))
+    const handleOnClick = () => {
+        const location = {
+            pathname: "/my-profile"
+        };
+        history.push(location);
+        window.location.reload();
+    };
+    const homePageOnClick = () => {
+        const location = {
+            pathname: "/"
+        };
+        history.push(location);
+        window.location.reload();
+    };
+    // const menu = list.map(item => (
+    //     <li key={item.name}>
+    //         <NavLink to={item.path} exact = {item.exact ? item.exact : false }>{item.name}</NavLink>
+    //     </li>
+    // ))
 
     return (
             <NavList>
-                {menu}
+                {/* {menu} */}
+                <NavButton onClick={homePageOnClick}>Start</NavButton>
+                <NavButton onClick={handleOnClick}>My profile</NavButton>    
             </NavList>
     );
 }
-//export default connect(mapStateToProps, null, null, {pure: false})(Navigation);
+
 export default Navigation;
